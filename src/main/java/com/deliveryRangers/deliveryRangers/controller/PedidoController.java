06f0162e1fb80f1aa4154104a53f95a8e5f1b55f
mozartCuman <mozartcumana@gmail.com>
2024-12-03 15:01:48 -0300

    Adição na classe Pedido dos métodos getAll(), getById() e getByDescricao

--- src/main/java/com/deliveryRangers/deliveryRangers/controller/PedidoController.java
+++ src/main/java/com/deliveryRangers/deliveryRangers/controller/PedidoController.java
@@ -1,5 +1,42 @@
 package com.deliveryRangers.deliveryRangers.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.deliveryRangers.deliveryRangers.model.Pedido;
+import com.deliveryRangers.deliveryRangers.repository.PedidoRepository;
+
+@RestController
+@RequestMapping("/pedido")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PedidoController {
 
+	@Autowired 
+	private PedidoRepository pedidoRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Pedido>> getAll() {
+		return ResponseEntity.ok(pedidoRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Pedido> getById(@PathVariable Long id) {
+		return pedidoRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	
+	@GetMapping("/descricao/{descricao}")
+	public ResponseEntity<List<Pedido>> getByTitle(@PathVariable String descricao) {
+		return ResponseEntity.ok(pedidoRepository.findAllBydescricaoContainingIgnoreCase(descricao));
+	}
+	
 }
