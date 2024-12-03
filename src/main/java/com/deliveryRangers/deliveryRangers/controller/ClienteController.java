1933d54ba563471ce826a4da8d7e77c203433cd1
mozartCuman <mozartcumana@gmail.com>
2024-12-03 15:49:35 -0300

    Adição de getall Getbyid getClient lyncolnAzul

--- src/main/java/com/deliveryRangers/deliveryRangers/controller/ClienteController.java
+++ src/main/java/com/deliveryRangers/deliveryRangers/controller/ClienteController.java
@@ -1,5 +1,43 @@
 package com.deliveryRangers.deliveryRangers.controller;
 
-public class ClienteController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.deliveryRangers.deliveryRangers.model.Cliente;
+import com.deliveryRangers.deliveryRangers.repository.ClienteRepository;
+
+@RestController
+@RequestMapping("/cliente")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ClienteController {
+	
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Cliente>> getAll(){
+	return ResponseEntity.ok(clienteRepository.findAll());
+	
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> getById(@PathVariable Long id) {
+	return clienteRepository.findById(id)
+	.map(resposta -> ResponseEntity.ok(resposta))
+	.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Cliente>> getByNome(@PathVariable String nome){
+	return ResponseEntity.ok(clienteRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
 }
